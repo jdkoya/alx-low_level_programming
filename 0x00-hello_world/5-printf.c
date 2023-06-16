817d25b5a5a72c3ed8b1b7a269c5821060fc5fff
jdkoya <jerome9771413@gmail.com>
2023-06-16 00:38:58 +0000

    printing with printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A simple programme that prints a statement using 'printf' command
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
+
