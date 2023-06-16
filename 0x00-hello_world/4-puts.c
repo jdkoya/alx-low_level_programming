43395105a0751d94c6b62e8e93757206f0786617
jdkoya <jerome9771413@gmail.com>
2023-06-16 00:24:14 +0000

    printing with puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main - A simple programme that prints a statement using 'put' command
- * Return: 0
+/**
+ * main - A simple programme that prints a statement using'put'command
+ * Return: 0 (successful)
  */
-int main(void)
+int main(void) /* main funtion */
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
