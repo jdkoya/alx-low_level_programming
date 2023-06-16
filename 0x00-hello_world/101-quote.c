ea9fe15528811e16dcc73c0398701678a46d78ec
jdkoya <jerome9771413@gmail.com>
2023-06-16 03:50:51 +0000

    print a line to a standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  * main - A programme that prints a line to standard error 
+ *   * Return: 1 (seccessful)
+ *   */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
