b4abfb2dea523b536d6e0da94f909052a62d8600
jdkoya <jerome9771413@gmail.com>
2023-06-17 01:26:07 +0000

    for loop 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints the size of variables types
+ * Return: 0 (seccessful)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int c;
+
+	for (c = 0; c < 52; c++)
+	{
+	putchar(alp[c]);
+	}
+	putchar('\n');
+	return (0);
+}
