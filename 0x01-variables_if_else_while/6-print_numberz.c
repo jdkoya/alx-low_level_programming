a67c506b385eabcb989b5558032fb4decba60626
jdkoya <jerome9771413@gmail.com>
2023-06-17 02:45:23 +0000

    for loop only 2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints all single digits of base 10
+ * Return: 0 (seccessful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
