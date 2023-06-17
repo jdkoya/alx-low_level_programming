7537ca6546a4813181cd050390806ea4ff41ad12
jdkoya <jerome9771413@gmail.com>
2023-06-17 02:01:53 +0000

    for loop and if loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - A programme that prints letter without e and q
+ * Return: 0 (seccessful)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 26; c++)
+	{
+		if (c != 4 && c != 16)
+		{
+		putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
