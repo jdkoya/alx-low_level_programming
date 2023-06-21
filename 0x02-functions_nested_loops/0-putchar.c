7c77369ba0dad254846c34b0392ac1bacd5df05b
jdkoya <jerome9771413@gmail.com>
2023-06-21 17:36:07 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a word
+ * Return: 0
+ */
+
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** programme that prints the size of variables types
+  * Return: 0 (seccessful)
+  */
+
+void print_alphabet(void);
+{
+	char k;
+
+	for(k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+	_putchar('\n');
+}
