7d33167d015285199a3778fdf4ef43645aa54181
jdkoya <jerome9771413@gmail.com>
2023-06-22 02:28:20 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - A programme that prints absolute values
+ * Return: 0
+ * @int: is an integer
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
