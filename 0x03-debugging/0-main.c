03a41f2b4ba4009f3b2a8fa364b72486b16cf62d
jdkoya <jerome9771413@gmail.com>
2023-06-23 00:21:36 +0000

    Testing function in main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Testing function positive_or_negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_nagitive(i);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+void positive_or_negative(int i);
+#endif
