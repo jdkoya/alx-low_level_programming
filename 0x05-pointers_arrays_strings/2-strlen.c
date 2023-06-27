4bcf60f33012e96f35f0e6cf01c67e83aeddc6dd
jdkoya <jerome9771413@gmail.com>
2023-06-27 11:37:13 +0000

    swap values of integers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - A programme that returns the length of a string
+ * @s: a pointer
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	strlen(str);
+}
