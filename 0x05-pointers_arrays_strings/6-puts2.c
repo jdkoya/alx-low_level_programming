d4496e33c35d7425d52f2bcd58ef09f9db81c657
jdkoya <jerome9771413@gmail.com>
2023-06-27 23:47:40 +0000

    print even numbers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
