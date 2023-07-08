0b6b29774ee70db4c308a44dcbdf0a753aef9a4e
jdkoya <jerome9771413@gmail.com>
2023-07-08 05:42:56 +0000

    adds integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = 0;
+	int num2 = 0;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 + num2);
+	}
+	else if (argc == 1)
+		printf("0\n");
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
