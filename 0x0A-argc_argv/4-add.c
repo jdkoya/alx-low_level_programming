8c2a13d1cd977016e8e8b373d140bf51e4dd4dc9
jdkoya <jerome9771413@gmail.com>
2023-07-09 15:29:18 +0000

    adds integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,21 @@ int main(int argc, char *argv[])
 {
 	int num1 = 0;
 	int num2 = 0;
+	int num3, num4;
 
 	if (argc == 3)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		printf("%d\n", num1 + num2);
+	}
+	else if ( argc == 5)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		num3 = atoi(argv[3]);
+		num4 = atoi(argv[4]);
+		printf("%d\n", num1 + num2 + num3 + num4);
 	}
 	else if (argc == 1)
 		printf("0\n");
