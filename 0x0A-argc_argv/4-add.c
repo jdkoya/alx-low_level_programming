801518a17834cb0e62bf300938541834ba8d57e7
jdkoya <jerome9771413@gmail.com>
2023-07-09 22:59:35 +0000

    adds integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - adds integers
@@ -10,30 +11,32 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0;
-	int num2 = 0;
-	int num3 = 0, num4 = 0;
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
 
-	if (argc == 3)
+	if (argc > 1)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		printf("%d\n", num1 + num2);
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(e);
+			e++;
+		}
+		printf("%d\n", sum);
 	}
-	else if (argc == 5)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		num3 = atoi(argv[3]);
-		num4 = atoi(argv[4]);
-		printf("%d\n", num1 + num2 + num3 + num4);
-	}
-	else if (argc == 1)
-		printf("0\n");
 	else
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
 	}
-	return (0);
+return (0);
 }
