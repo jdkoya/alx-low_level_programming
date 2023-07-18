6a79ed86ecfc1cd98a1f0b17d8fdc91ad2567ca7
jdkoya <jerome9771413@gmail.com>
2023-07-18 21:41:09 +0000

    initialising structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * struct dog - about my dog
+ * @d: the to be initialised
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: a structure that is use to collect data of my dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
+	}
+}
+
