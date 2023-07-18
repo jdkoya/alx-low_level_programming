a8732eecfa936c5f7bf6582254856a40decb959f
jdkoya <jerome9771413@gmail.com>
2023-07-18 01:06:50 +0000

    creating poppy structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+#include "dog.h"
+
+/**
+ * struct dog - about my dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: a structure that is use to collect data of my dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
