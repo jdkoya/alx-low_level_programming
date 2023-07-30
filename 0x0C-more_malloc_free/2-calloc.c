8a30106628f593dbf26962d2c1cb577e85e6f8e2
jdkoya <jerome9771413@gmail.com>
2023-07-30 15:11:54 +0000

    a function that allocates memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,25 @@
 #include "main.h"
 
 /**
-  *_calloc - a function that allocates memory
+ * _memset - a function that creates a constant byte
+ * @s: pointer to memory
+ * @b: character
+ * @n: integer
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
+ *_calloc - a function that allocates memory
  * @nmemb: argument
  * @size: size of argument
  * Return: allocated memory
@@ -17,6 +35,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (0);
+	_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
 
